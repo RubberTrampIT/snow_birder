fe139bdd777af98f78daa035cce5e2f33ef804bd
RubberTrampIT <rubbertrampit@gmail.com>
2021-12-18 12:21:06 -0700

    Update get_historical_weather_data.py

--- noaa_api/scripts/get_historical_weather_data.py
+++ noaa_api/scripts/get_historical_weather_data.py
@@ -3,4 +3,10 @@ import requests, json
 response = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/data?limit=100&offset=0&datasetid=normal_dly&datatypeid=dly-tmin-normal&datatypeid=dly-tmax-normal&datatypeid=dly-tavg-normal&datatypeid=dly-prcp-normal&stationid=GHCND%3AUSC00020060&startdate=2010-02-01&enddate=2010-02-28T07%3A00%3A00.000', headers={'token': 'FqVETqfxQksyfJYSwEeGvMIeQiliHRct'})
 data = json.loads(response.text)
 
-print(data["results"][0])
+for result in data["results"]:
+    data_date = result["date"]
+    data_datatype = result["datatype"]
+    data_station = result["station"]
+    data_value = float(result["value"]/10)
+    print (data_value)
+#print (data)
