de71c2270a7343de7b96bc2049270c1f8a31597e
RubberTrampIT <rubbertrampit@gmail.com>
2021-12-19 10:19:35 -0700

    Added list of ghcnd stations and added date regex

--- noaa_api/scripts/get_historical_weather_data.py
+++ noaa_api/scripts/get_historical_weather_data.py
@@ -1,12 +1,30 @@
-import requests, json
+import requests, json, re
+
 
 response = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/data?limit=100&offset=0&datasetid=normal_dly&datatypeid=dly-tmin-normal&datatypeid=dly-tmax-normal&datatypeid=dly-tavg-normal&datatypeid=dly-prcp-normal&stationid=GHCND%3AUSC00020060&startdate=2010-02-01&enddate=2010-02-28T07%3A00%3A00.000', headers={'token': 'FqVETqfxQksyfJYSwEeGvMIeQiliHRct'})
 data = json.loads(response.text)
+#print (data["results"][0]["station"])
+
+
 
 for result in data["results"]:
     data_date = result["date"]
+    x = re.findall("\d{2}", data_date)
+    data_datemonth = x[2]
+    data_dateday = x[3]
+
     data_datatype = result["datatype"]
+    #Three datatypes
+    # DLY-TMAX-NORMAL
+    # DLY-TMIN-NORMAL
+    # DLY-TAVG-NORMAL
+
     data_station = result["station"]
-    data_value = float(result["value"]/10)
-    print (data_value)
-#print (data)
+    data_value = float(result["value"]/10) #they store the float as a 3-digit integer in their API must convert to real float
+
+    print (data_dateday)
+
+    #print (type(data_date))
+
+    #if data_datatype == "DLY-TMAX-NORMAL":
+    #    print (data_value)
